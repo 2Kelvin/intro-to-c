12efa1a6cae45100ef2500fac98f6b4ee897f5b6
2Kelvin <rocketmancodes02@gmail.com>
2023-03-01 22:25:20 +0300

    Project Cards complete The loop breaks if the user types X The loop asks for a valid number if the user enters a number not in the range of 1..10

--- README.md
+++ README.md
@@ -8,7 +8,8 @@ gcc filename.c -o filename && ./filename
 
 The `switch` statement is an efficient replacement of consequent if statements. It can check a variable or a value, but **it can only check only one single value.** This means that, **it cannot check multiple character strings or arrays.**
 
-Normally, functions have to `return` something. If this is not possible, they should have the return type `void`. Void functions have no return statements. The return type is defined before the function name while the actual return statement, in the function's body.
+Normally, functions have to `return` something. If this is not possible, they should have the return type `void`. Void functions have no return statements. The return type is defined before the function name while the actual return statement, in the function's body
+
     ```
     void function happy() 
     {
--- cards.c
+++ cards.c
@@ -3,11 +3,16 @@
 int main()
 {
     char card_name[3];
-    puts("Enter the card name: ");
-    scanf("%2s", card_name);
-    int val = 0;
+    int count = 0;
 
-    switch (card_name[0]){
+    while (card_name[0] != 'X')
+    {
+        puts("Enter the card name: ");
+        scanf("%2s", card_name);
+        int val = 0;
+
+        switch (card_name[0])
+        {
         case 'K':
         case 'Q':
         case 'J':
@@ -16,19 +21,26 @@ int main()
         case 'A':
             val = 11;
             break;
+        case 'X':
+            continue;
         default:
             val = atoi(card_name);
-    }
+            if (val < 1 || val > 10)
+            {
+                puts("Error! Enter a valid number");
+                continue;
+            }
+        }
 
-    // checking if the value is 3-6
-    if (val > 2 && val < 7)
-    {
-        puts("Count has gone up");
-    }
-    // checking if the card is 10, K, Q or J
-    else if (val == 10)
-    {
-        puts("Count has gone down");
+        // checking if the value is 3-6
+        if (val > 2 && val < 7)
+            count++;
+
+        // checking if the card is 10, K, Q or J
+        else if (val == 10)
+            count--;
+
+        printf("Your count is %i\n", count);
     }
 
     return 0;
